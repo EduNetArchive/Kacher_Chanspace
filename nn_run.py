d41dc9e7452acabedb5b4375003b59057b7dbfae
Julia Kacher <juliakacher007@gmail.com>
2022-06-26 16:45:01 +0200

    Fix bug with traj flag

--- nn_run.py
+++ nn_run.py
@@ -401,7 +401,7 @@ if __name__ == "__main__":
     parser.add_argument("--output", "-o", type=str, default="/home/ebam/kacher1/molearn/DATA")
     parser.add_argument("--experiment-name", "-e", type=str, default="all_kv1.2")
     parser.add_argument("--wandb", "-v", action="store_true", default=False)
-    parser.add_argument("--qcharge_file", "-q", type=str, default=False)
+    parser.add_argument("--qcharge-file", "-q", type=str, default=False)
     parser.add_argument("--traj", "-t", type=str, default=False)
 
     args = parser.parse_args()
